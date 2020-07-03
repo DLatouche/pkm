15583da63654a9f13d2754feafa0462109d34475
Dorian Latouche <d.latouche@betterfly-tourism.com>
2020-07-03 10:09:27 +0200

    init module of type, boxes and pokemons

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -3,12 +3,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TrainersModule } from './trainers/trainers.module';
+import { BoxesModule } from './boxes/boxes.module';
+import { TypesModule } from './types/types.module';
+import { PokemonsModule } from './pokemons/pokemons.module';
+import { TypeService } from './type/type.service';
 
 
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/pkm'), TrainersModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/pkm'), TrainersModule, BoxesModule, TypesModule, PokemonsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TypeService],
 })
 export class AppModule { }
