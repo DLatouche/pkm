847aa47a78d19953e7b0c61b4233ad763e23bbdd
Dorian Latouche <d.latouche@betterfly-tourism.com>
2020-07-03 10:06:38 +0200

    init trainers module and init files schema

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TrainersModule } from './trainers/trainers.module';
 
 
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/pkm')],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/pkm'), TrainersModule],
   controllers: [AppController],
   providers: [AppService],
 })
