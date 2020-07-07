ca0f3678dbebc4aa76a439d58409078b85b648b1
Dorian Latouche <d.latouche@betterfly-tourism.com>
2020-07-07 11:28:55 +0200

    Adding test for pokemonsController

--- back/src/pokemons/pokemons.controller.spec.ts
+++ back/src/pokemons/pokemons.controller.spec.ts
@@ -1,15 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PokemonsController } from './pokemons.controller';
+import { PokemonsService } from './pokemons.service';
 
-describe('Pokemons Controller', () => {
-  let controller: PokemonsController;
+describe('Pokemon Controller', () => {
+  let pokemonsController: PokemonsController;
+  let pokemonsService: PokemonsService;
+
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [PokemonsController],
-    }).compile();
-
-    controller = module.get<PokemonsController>(PokemonsController);
+    pokemonsService = { findAll: jest.fn() } as any;
+    pokemonsController = new PokemonsController(pokemonsService);
   });
 
+
+  describe('getAll', () => {
+    it('should return an array of pokemon', async () => {
+      const result = [{ id: "idPokemon", name: "namePokemon" }];
+      (pokemonsService.findAll as any).mockResolvedValue(result);
+      expect(await pokemonsController.getAll()).toBe(result);
+    });
+  });
 });
