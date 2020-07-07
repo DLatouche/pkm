8d88efb6aa7436a6bf55f901f7aac89bea714fb6
Dorian Latouche <d.latouche@betterfly-tourism.com>
2020-07-07 11:23:29 +0200

    Adding test for typesController

--- back/src/types/types.controller.spec.ts
+++ back/src/types/types.controller.spec.ts
@@ -1,15 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypesController } from './types.controller';
+import { TypesService } from './types.service';
 
 describe('Types Controller', () => {
-  let controller: TypesController;
+  let typesController: TypesController;
+  let typesService: TypesService;
+
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [TypesController],
-    }).compile();
-
-    controller = module.get<TypesController>(TypesController);
+    typesService = { create: jest.fn(), findAll: jest.fn() } as any;
+    typesController = new TypesController(typesService);
   });
 
+  describe('create', () => {
+    it('should return a type', async () => {
+      const result = { id: "idType", name: "nameType" };
+      (typesService.create as any).mockResolvedValue(result);
+      expect(await typesController.create("name")).toBe(result);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of type', async () => {
+      const result = [{ id: "idType", name: "nameType" }];
+      (typesService.findAll as any).mockResolvedValue(result);
+      expect(await typesController.findAll()).toBe(result);
+    });
+  });
 });
